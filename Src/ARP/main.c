ab42e57bb5d16db4237039b21085a6e9343e11f9
TT375S <tomataka375@gmail.com>
2017-12-25 19:16:02 +0900

    Fix pcap dump format. It completely well work.

--- Src/ARP/main.c
+++ Src/ARP/main.c
@@ -15,7 +15,7 @@
 #include    <pthread.h>
 #include	"netutil.h"
 #include    <unistd.h>
-#include <sys/time.h>
+#include    <sys/time.h>
 
 uint8_t bytes[6];
 
@@ -126,13 +126,13 @@ struct pcap_file_header{
     uint32_t magic;
     uint16_t version_major;
     uint16_t version_minor;
-    int32_t thiszone;
+    int32_t thiszone;   //タイムゾーン。GMT+1:00なら-3600になるらしいが、tcpdumpはここを0でダンプするのでそれに合わせる
     uint32_t sigfigs;
-    uint32_t snaplen;
+    uint32_t snaplen;   //最大パケット長。0xFF、つまり65535を指定するらしいが、このプログラムは2048までしか対応していない
     uint32_t linktype;
 };
 
-char pcapDumpFileName[50] = "pcapDump.pcap";
+char pcapDumpFileName[50] = "pcapDump.pcap";    //保存するpcapファイル名
 
 //pcapファイルを準備
 void pcap_init(){
@@ -140,7 +140,7 @@ void pcap_init(){
     pfhdr->magic = TCPDUMP_MAGIC;
     pfhdr->version_major = PCAP_VERSION_MAJOR;
     pfhdr->version_minor = PCAP_VERSION_MINOR;
-    pfhdr->thiszone = -1*9*60*60;   //GMT+1:00だと-3600になるらしい
+    pfhdr->thiszone = 0;   //前述の通り、tcpdumpに合わせて0にしてしまう
     pfhdr->snaplen = 65535;
     pfhdr->sigfigs = 0;
     pfhdr->linktype = DLT_EN10MB;
@@ -154,7 +154,10 @@ void pcap_init(){
 
 //パケットの先頭に書き込む
 struct pcap_pkthdr{
-    struct timeval ts;
+    //struct timeval ts; //この構造体を用いると、x86とx64の違いなのか、ひとつ64bitで保存されてしまいズレる
+    uint32_t ts_sec;    //このように32bitで保存しとく
+    uint32_t ts_usec;
+
     uint32_t caplen;
     uint32_t len;
 };
@@ -162,8 +165,12 @@ struct pcap_pkthdr{
 //パケット追記する
 void pcap_write(u_char *data, int dsize){
     struct pcap_pkthdr pkthdr;
+    struct timeval ts;
     struct timezone tz;
-    gettimeofday(&(pkthdr.ts), &tz);
+    gettimeofday(&ts, &tz);
+
+    pkthdr.ts_sec = (uint32_t)ts.tv_sec;
+    pkthdr.ts_usec = (uint32_t)ts.tv_usec;
     pkthdr.caplen = dsize;
     pkthdr.len = dsize;
     
